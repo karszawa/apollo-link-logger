5e8dfd4ccd8177f7585f6325f72a2d6bf1214eb2
Hiroaki KARASAWA <karszawa@users.noreply.github.com>
2020-06-18 02:48:21 +0900

    feat: provide createLoggerLink

--- src/index.js
+++ src/index.js
@@ -3,23 +3,24 @@ import { ApolloLink } from 'apollo-link';
 import formatMessage from './formatMessage';
 import logging from './logging';
 
-const loggerLink = new ApolloLink((operation, forward) => {
-  const startTime = new Date().getTime();
+export const createLoggerLink = ({ stringify = false }) =>
+  new ApolloLink((operation, forward) => {
+    const startTime = new Date().getTime();
 
-  return forward(operation).map(result => {
-    const operationType = operation.query.definitions[0].operation;
-    const ellapsed = new Date().getTime() - startTime;
+    return forward(operation).map(result => {
+      const operationType = operation.query.definitions[0].operation;
+      const ellapsed = new Date().getTime() - startTime;
 
-    const group = formatMessage(operationType, operation, ellapsed);
+      const group = formatMessage(operationType, operation, ellapsed);
 
-    logging.groupCollapsed(...group);
+      logging.groupCollapsed(...group);
 
-    logging.log('INIT', operation);
-    logging.log('RESULT', result);
+      logging.log('INIT', stringify ? JSON.stringify(stringify) : operation);
+      logging.log('RESULT', stringify ? JSON.stringify(result) : result);
 
-    logging.groupEnd(...group);
-    return result;
+      logging.groupEnd(...group);
+      return result;
+    });
   });
-});
 
-export default loggerLink;
+export default createLoggerLink;
